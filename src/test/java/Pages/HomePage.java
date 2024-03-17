3b8fcd21ec5b744770a55fade8335cc1692f8c54
LaviniaGandila <laviniafacebook2@yahoo.com>
2024-03-17 18:41:36 +0200

    tot cu home page

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -18,8 +18,6 @@ public class HomePage extends BasePage {
     @FindBy(xpath = "//h5[text()='Alerts, Frame & Windows']")
     private WebElement alertFrameWindowsField;
 
-    @FindBy(xpath = "//h5[text()='Forms']")
-    private WebElement formsField;
 
     @FindBy(xpath = "//h5[text()='Elements']")
     private WebElement elementsField;
@@ -30,10 +28,6 @@ public class HomePage extends BasePage {
         elementMethods.clickElement(alertFrameWindowsField);
     }
 
-    public void navigateToFormsPage() {
-        elementMethods.scrollElementByPixel(0, 450);
-        elementMethods.clickElement(formsField);
-    }
 
     public void navigateToElementsPage() {
         elementMethods.scrollElementByPixel(0, 450);
